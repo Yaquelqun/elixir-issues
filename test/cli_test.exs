5e530dbc7c271a254a8aa7680c341207add29e0f
Yaquelqun <sandjiv.parassou@gmail.com>
2022-10-06 17:18:49 +0200

    add method to sortresult into correct order

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CLITest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [parse_args: 1] # we need to inport the things that we want to test
+  import Issues.CLI, only: [parse_args: 1, sort_into_descending_order: 1]
 
   test ":help is returned if we pass -h or --help as an option" do
     assert parse_args(["-h", "anything"]) == :help
@@ -15,5 +15,15 @@ defmodule CLITest do
 
   test "returns three values with default count if two values are given" do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "sort descending orders the correct way" do
+    result = sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{c b a}
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values, do: %{"created_at"=> value, "oter_data" => "xxx"}
   end
 end
